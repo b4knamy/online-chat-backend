f5898e38c8c730b33676635d6352f13e6d724e32
Rogério França <rogeriofranca.contato@gmail.com>
2024-12-30 00:57:54 -0300

    add models registry to admin page

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from chat.models import User
+from chat.models import User, Messages, Room
 
 # Register your models here.
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Messages)
+class MessagesAdmin(admin.ModelAdmin):
     pass
